e1aed643b7e4c301ff26bba9b5c028624aecac39
abaikb <abaikb@icloud.com>
2023-06-13 20:28:06 +0600

    add auth

---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleRememberMeChange = (e) => {
+        setRememberMe(e.target.checked);
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        // Здесь могут быть дополнительные проверки, например, проверка с сервером на соответствие имени пользователя и пароля
+
+        // Проверка успешности аутентификации
+        if (username === 'admin' && password === 'password') {
+            // Выполнение необходимых действий после успешного входа
+            console.log('Успешный вход в систему');
+        } else {
+            console.log('Неверное имя пользователя или пароль');
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleLogin}>
+                <input type="text" value={username} onChange={handleUsernameChange} placeholder="Номер телефона" />
+                <input type="password" value={password} onChange={handlePasswordChange} placeholder="Пароль" />
+                <span className="login-checkbox">
+                    <input className="checkbox" type="checkbox" checked={rememberMe} onChange={handleRememberMeChange} />
+                    <label></label>
+                    <span>Запомнить меня</span>
+                    <a className='forgot-link' href="#">Забыли пароль?</a>
+                </span>
+
+                <button type="submit">Войти</button>
+                <div className="login-links">
+                    <a className='link' href="#">Регистрация</a>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
