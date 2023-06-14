5f0342427cd44ab549ff443b82beb2926e98c484
AzizChiderov <chiderovaziz@gmail.com>
2023-06-14 16:57:03 +0600

    delete from layout

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Header  from '../header_/header'
 import Market  from '../market/index'
-import Footer  from '../footer/footer'
+// import Footer  from '../footer/footer'
 
 
 export const Layout = () => {
@@ -15,7 +15,7 @@ export const Layout = () => {
       
       <Market />
 
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
