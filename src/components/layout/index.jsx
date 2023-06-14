6c44496edcb2fcdf0161214eb34521935f2423e7
AzizChiderov <chiderovaziz@gmail.com>
2023-06-14 16:48:12 +0600

    fix files

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,12 +1,7 @@
 import { Outlet } from 'react-router-dom'
-<<<<<<< HEAD:src/components/layout/index.jsx
-import Header  from '../header /header'
+import Header  from '../header_/header'
 import Market  from '../market/index'
 import Footer  from '../footer/footer'
-=======
-import Header from '../header_/header'
-// import Footer  from '../footer/footer'
->>>>>>> 102c1e29897bd9c097461732a5db46444e1fb770:RSK-project-main/src/components/layout/index.jsx
 
 
 export const Layout = () => {
@@ -18,13 +13,9 @@ export const Layout = () => {
         <Outlet />
       </div>
       
-<<<<<<< HEAD:src/components/layout/index.jsx
       <Market />
 
       <Footer />
-=======
-      {/* <Footer /> */}
->>>>>>> 102c1e29897bd9c097461732a5db46444e1fb770:RSK-project-main/src/components/layout/index.jsx
     </>
   )
 }
