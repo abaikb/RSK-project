7b02ecbbdae803da0f21178d8749ef95d8836c6e
AzizChiderov <chiderovaziz@gmail.com>
2023-06-14 15:30:17 +0600

    add about us

--- RSK-project-main/src/components/layout/index.jsx
+++ RSK-project-main/src/components/layout/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Header  from '../header /header'
-import Footer  from '../footer/footer'
+import Header  from '../header_/header'
+// import Footer  from '../footer/footer'
 
 
 export const Layout = () => {
@@ -12,7 +12,7 @@ export const Layout = () => {
         <Outlet />
       </div>
       
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
