6d56c8d1f40c3a93a17d7c66ccf22b60be57ef8c
Samy4-byte <samatbayizov@gmail.com>
2023-06-14 16:55:35 +0600

    fix main

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { AboutPage } from "./pages/about_us";
 import { OrderPage } from "./pages/online_order";
-import { MainPage } from "./pages/glavnaya/main";
+import { MainPage } from "./pages/main/index";
 import RegistrationForm from "./pages/signup/signup";
 import "./App.css";
 
@@ -13,7 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
+            <Route path="/main" index element={<MainPage />} />
             <Route path="/about_us" element={<AboutPage />} />
             <Route path="/order" element={<OrderPage />} />
             <Route path="/order/signup" element={<RegistrationForm />} />
--- src/pages/glavnaya/main.jsx
+++
@@ -1,8 +0,0 @@
-
-export const MainPage = () => {
-  return (
-    <div>
-      Главная
-    </div>
-  );
-};
