b9c2cf87ddf6637ef2bd3df5db34c9741f65527d
abaikb <abaikb@icloud.com>
2023-06-15 15:31:31 +0600

    ewffewfwf

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,57 +1,80 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css';
+import L from "./login.module.css";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [rememberMe, setRememberMe] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-    };
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleRememberMeChange = (e) => {
-        setRememberMe(e.target.checked);
-    };
+  const handleRememberMeChange = (e) => {
+    setRememberMe(e.target.checked);
+  };
 
-    const handleLogin = (e) => {
-        e.preventDefault();
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-        // Здесь могут быть дополнительные проверки, например, проверка с сервером на соответствие имени пользователя и пароля
+    // Здесь могут быть дополнительные проверки, например, проверка с сервером на соответствие имени пользователя и пароля
 
-        // Проверка успешности аутентификации
-        if (username === 'admin' && password === 'password') {
-            // Выполнение необходимых действий после успешного входа
-            console.log('Успешный вход в систему');
-        } else {
-            console.log('Неверное имя пользователя или пароль');
-        }
-    };
+    // Проверка успешности аутентификации
+    if (username === 'admin' && password === 'password') {
+      // Выполнение необходимых действий после успешного входа
+      console.log('Успешный вход в систему');
+    } else {
+      console.log('Неверное имя пользователя или пароль');
+    }
+  };
 
-    return (
-        <div className="login-container">
-            <form className="login-form" onSubmit={handleLogin}>
-                <input type="text" value={username} onChange={handleUsernameChange} placeholder="Номер телефона" />
-                <input type="password" value={password} onChange={handlePasswordChange} placeholder="Пароль" />
-                <span className="login-checkbox">
-                    <input className="checkbox" type="checkbox" checked={rememberMe} onChange={handleRememberMeChange} />
-                    <label></label>
-                    <span>Запомнить меня</span>
-                    <a className='forgot-link' href="#">Забыли пароль?</a>
-                </span>
+  return (
+    <div className={L.login_container}>
+      <form className={L.login_form} onSubmit={handleLogin}>
+        <input
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder="Номер телефона"
+          className={L.login_input}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Пароль"
+          className={L.login_input}
+        />
+        <span className={L.login_checkbox}>
+          <input
+            className={L.checkbox}
+            type="checkbox"
+            checked={rememberMe}
+            onChange={handleRememberMeChange}
+          />
+          <label></label>
+          <span>Запомнить меня</span>
+          <a className={L.forgot_link} href="#">
+            Забыли пароль?
+          </a>
+        </span>
 
-                <button type="submit">Войти</button>
-                <div className="login-links">
-                    <Link className='link' to='./signup'><a  href="#">Регистрация</a></Link>
-                </div>
-            </form>
+        <button type="submit" className={L.login_button}>
+          Войти
+        </button>
+        <div className={L.login_links}>
+          <Link className={L.link} to="./signup">
+            <a className={L.link} href="#">Регистрация</a>
+          </Link>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default Login;
