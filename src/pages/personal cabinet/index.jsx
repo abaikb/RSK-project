fca939d677b1172034faab1f4d73babd9e48522c
AzizChiderov <chiderovaziz@gmail.com>
2023-06-22 16:19:02 +0600

    fix

--- src/pages/personal cabinet/index.jsx
+++ src/pages/personal cabinet/index.jsx
@@ -4,6 +4,7 @@ import Banner from '../../components/images/banner.png'
 import Avatar from '../../components/images/Ellipse 171.png'
 import Pen from '../../components/images/pen.svg'
 import wPen from '../../components/images/white_pen.svg'
+import pin from '../../components/images/pin.svg'
 
 export const Personal = () => {
   return (
@@ -26,7 +27,17 @@ export const Personal = () => {
         <input type="text" placeholder='Изменить пароль' src={Pen} />
         <div className={p.ticket_box}>
           <h3>Текущий билет</h3>
-          <button className={p.ticket}></button>
+          <div className={p.ticket}>
+            <img src={pin} alt="#" />
+            <div className={p.adress}>
+              <span>Сберкасса № 36-56 </span>
+              <span>г.Бишкек  ул.Киевская №165 </span>
+            </div>
+            <div className={p.data}>
+              <span>12/06</span>
+              <span>09:00</span>
+            </div>
+          </div>
           <a className={p.history} href="#">История</a>
         </div>
       </div>
